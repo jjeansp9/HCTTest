2b97fff0feeb2463bb40c2d85cdd2e3b101c4664
박진솔 <jjeansp9@gmail.com>
2023-04-20 10:40:58 +0900

    commit

--- app/src/main/java/kr/co/testapp0501/view/activities/SignUpActivity.kt
+++ app/src/main/java/kr/co/testapp0501/view/activities/SignUpActivity.kt
@@ -1,6 +1,7 @@
 package kr.co.testapp0501.view.activities
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,6 +12,8 @@ import android.text.TextUtils
 import android.text.style.RelativeSizeSpan
 import android.view.MenuItem
 import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import kr.co.testapp0501.R
@@ -29,6 +32,7 @@ class SignUpActivity : AppCompatActivity() {
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
         clickedNext()
+        clickedBackGround()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -46,11 +50,17 @@ class SignUpActivity : AppCompatActivity() {
 
         // 성별 버튼 클릭 [남자]
         binding.btnMan.setOnClickListener{
-
+            it.setBackgroundResource(R.drawable.bg_gender_man)
+            binding.btnMan.setTextColor(ContextCompat.getColor(this, R.color.white))
+            binding.btnGirl.setBackgroundResource(R.drawable.bg_gender_girl_white)
+            binding.btnGirl.setTextColor(ContextCompat.getColor(this, R.color.black))
         }
         // 성별 버튼 클릭 [여자]
         binding.btnGirl.setOnClickListener{
-
+            it.setBackgroundResource(R.drawable.bg_gender_girl)
+            binding.btnGirl.setTextColor(ContextCompat.getColor(this, R.color.white))
+            binding.btnMan.setBackgroundResource(R.drawable.bg_gender_man_white)
+            binding.btnMan.setTextColor(ContextCompat.getColor(this, R.color.black))
         }
 
         // 휴대폰번호 인증요청 버튼 클릭
@@ -87,5 +97,27 @@ class SignUpActivity : AppCompatActivity() {
                 else -> false
             }
         }
+    }
+
+
+
+    // 키보드가 열린 상태일 때
+    // 키보드 외의 바깥 영역을 클릭하면 키보드 내려가게하는 메소드
+    @SuppressLint("ClickableViewAccessibility")
+    private fun clickedBackGround(){
+        binding.signUpRoot.setOnTouchListener(View.OnTouchListener { v, event ->
+            if (v.hasFocus()) {
+                val inputManager: InputMethodManager =
+                    this.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                inputManager.hideSoftInputFromWindow(
+                    this.currentFocus!!.windowToken,
+                    InputMethodManager.HIDE_NOT_ALWAYS
+                )
+                false
+
+            } else {
+                true
+            }
+        })
     }
 }
