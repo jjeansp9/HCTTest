a8e4579c0429a56e12d1455d87b0ec1b210e27bd
박진솔 <jjeansp9@gmail.com>
2023-05-02 15:20:23 +0900

    commit

--- app/src/main/java/kr/co/testapp0501/view/activity/GroupCreateActivity.kt
+++ app/src/main/java/kr/co/testapp0501/view/activity/GroupCreateActivity.kt
@@ -30,12 +30,7 @@ import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody.Companion.asRequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
-import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import java.io.File
-
 
 class GroupCreateActivity : AppCompatActivity() {
 
