2e3333d99d9632ec56ee8368930ed08640e3c39a
박진솔 <jjeansp9@gmail.com>
2023-05-12 16:56:37 +0900

    commit

--- app/src/main/java/kr/co/testapp0501/view/adapter/RecyclerAlbumActivityAdapter.kt
+++ app/src/main/java/kr/co/testapp0501/view/adapter/RecyclerAlbumActivityAdapter.kt
@@ -26,7 +26,6 @@ class RecyclerAlbumActivityAdapter constructor(private val context: Context, pri
         return VH(itemView)
     }
 
-    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: VH, position: Int) {
 
         // 앨범 관련
@@ -35,46 +34,47 @@ class RecyclerAlbumActivityAdapter constructor(private val context: Context, pri
         holder.binding.tvAlbumTitle.text = items[position].albumTitle // 제목
         holder.binding.tvAlbumContents.text = items[position].albumContents // 내용
 
+        setAlbumPicture(holder, position)
+    }
+    override fun getItemCount(): Int = items.size
+
+    // 앨범 이미지 갯수마다 화면 UI 변경
+    private fun setAlbumPicture(holder: VH, position: Int){
+
+        val albumItem = items[position]
+
         // 등록된 프로필이 없을 때
-        if (items[position].albumProfileImg == "") Glide.with(context).load(R.drawable.img_profile).into(holder.binding.imgAlbumProfile)
-        // 등록된 프로필이 있을 때
-        else Glide.with(context).load(items[position].albumProfileImg).into(holder.binding.imgAlbumProfile)
+        when {
+            albumItem.albumUploadPicture.isEmpty() -> {
+                Glide.with(context).load(R.drawable.img_group_general).into(holder.binding.imgAlbumUploadPictureFirst)
+            }
+            albumItem.albumUploadPicture.size == 1 -> {
+                Glide.with(context).load(albumItem.albumUploadPicture[0]).into(holder.binding.imgAlbumUploadPictureFirst)
+            }
+            albumItem.albumUploadPicture.size == 2 -> {
+                Glide.with(context).load(albumItem.albumUploadPicture[0]).into(holder.binding.imgAlbumUploadPictureFirst)
+                Glide.with(context).load(albumItem.albumUploadPicture[1]).into(holder.binding.imgAlbumUploadPictureSecond)
+                holder.binding.imgAlbumPictureLine.visibility = View.VISIBLE
+                holder.binding.layoutAlbumUploadPictureSecond.visibility = View.VISIBLE
+            }
+            else -> {
+                val count = albumItem.albumUploadPicture.size - 2
+                holder.binding.tvPictureCount.text = count.toString()
 
-        val count : Int
+                Glide.with(context).load(albumItem.albumUploadPicture[0]).into(holder.binding.imgAlbumUploadPictureFirst)
+                Glide.with(context).load(albumItem.albumUploadPicture[1]).into(holder.binding.imgAlbumUploadPictureSecond)
 
-        // 등록된게 없을 때 [업로드 사진]
-        if (items[position].albumUploadPicture.isEmpty()) Glide.with(context).load(R.drawable.img_group_general).into(holder.binding.imgAlbumUploadPictureFirst)
+                holder.binding.imgAlbumPictureLine.visibility = View.VISIBLE
+                holder.binding.tvPictureCount.visibility = View.VISIBLE
+                holder.binding.layoutAlbumUploadPictureSecond.visibility = View.VISIBLE
 
-        else if (items[position].albumUploadPicture.size == 1){ // 이미지가 1개일 때
-            Glide.with(context).load(items[position].albumUploadPicture[0]).into(holder.binding.imgAlbumUploadPictureFirst)
-        }
-        // 등록된게 있을 때 [업로드 사진]
-        else if (items[position].albumUploadPicture.size == 2){ // 이미지가 2개일 때
-            Glide.with(context).load(items[position].albumUploadPicture[0]).into(holder.binding.imgAlbumUploadPictureFirst)
-            Glide.with(context).load(items[position].albumUploadPicture[1]).into(holder.binding.imgAlbumUploadPictureSecond)
-            holder.binding.imgAlbumPictureLine.visibility = View.VISIBLE
-            holder.binding.layoutAlbumUploadPictureSecond.visibility = View.VISIBLE
-        }
-        else if (items[position].albumUploadPicture.size > 2){ // 이미지가 2개 이상일 때
-            count = items[position].albumUploadPicture.size - 2
-            holder.binding.imgPictureCount.text = "+ $count"
-
-            Glide.with(context).load(items[position].albumUploadPicture[0]).into(holder.binding.imgAlbumUploadPictureFirst)
-            Glide.with(context).load(items[position].albumUploadPicture[1]).into(holder.binding.imgAlbumUploadPictureSecond)
-
-            holder.binding.imgAlbumPictureLine.visibility = View.VISIBLE
-            holder.binding.imgPictureCount.visibility = View.VISIBLE
-            holder.binding.layoutAlbumUploadPictureSecond.visibility = View.VISIBLE
-
-            // 업로드된 2번째 이미지의 밝기조절
-            holder.binding.imgAlbumUploadPictureSecond.colorFilter = LightingColorFilter(
-                ContextCompat.getColor(context, R.color.picture_dark),
-                ContextCompat.getColor(context, R.color.picture_alpha)
-            )
+                holder.binding.imgAlbumUploadPictureSecond.colorFilter = LightingColorFilter(
+                    ContextCompat.getColor(context, R.color.picture_dark),
+                    ContextCompat.getColor(context, R.color.picture_alpha)
+                )
+            }
         }
     }
-
-    override fun getItemCount(): Int = items.size
 }
 
 
